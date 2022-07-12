d15b03a50bad552fcb75f4f85638f6802268992d
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-07-12 19:02:11 +0530

    Created Vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className="App">
+       Masai LMS
+    </h1>
+  )
+}
+
+export default App
