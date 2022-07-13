2f7047c0891ad25a646198dc688842f381c54763
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-07-13 16:41:23 +0530

    post functionality on UI

--- src/components/AdminPages/Assignments.jsx
+++ src/components/AdminPages/Assignments.jsx
@@ -1,40 +1,68 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Assignments = () => {
+
+  const [assignment, setAssignment] = useState(
+    {
+        topic:"",
+        creator:"",
+        codingDSA:"",
+        date:"",
+        time:"",
+        link:"",
+        password:""
+    }
+)
+
+const handleChange = (e) => {
+    const {id, value} = e.target
+   
+    setAssignment({...assignment, [id]:value})
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+
+    console.log('yes')
+    console.log(assignment)
+}
+
+
   return (
     <>
         <div className='mainBox'>
           <h1 className='nameHead'>Assignments</h1>
-              <form>
+              <form onSubmit={handleSubmit}>
 
                 <div className='divideInput'>
                   <div><label>Topic Name :</label></div>
-                  <div><input type="text" className='inpBox' /></div>
+                  <div><input type="text" className='inpBox' id='topic' onInput={handleChange} /></div>
                 </div>
 
                 <div className='divideInput'> 
                   <div><label>Creator Name</label></div>
-                  <div><input type="text" className='inpBox'  /></div>
+                  <div><input type="text" className='inpBox' id='creator'  onInput={handleChange}  /></div>
                 </div>
                 <div className='divideInput'>
-                  <div><label>Topic</label></div>
-                  <div><input type="text" className='inpBox' /></div>
+                  <div><label>Topic  ( Coding / DSA )</label></div>
+                  <div><input type="text" className='inpBox' id='codingDSA'  onInput={handleChange} /></div>
                 </div>
                 <div className='divideInput'>
                   <div><label>Date</label></div>
-                  <div><input type="Date" className='inpBox'  /></div>
+                  <div><input type="Date" className='inpBox' id='date' onInput={handleChange}  /></div>
                 </div>
                 <div className='divideInput'>
                   <div><label>Time</label></div>
-                  <div><input type="text" className='inpBox'  /></div>
+                  <div><input type="text" className='inpBox' id='time' onInput={handleChange}  /></div>
                 </div>
                 <div className='divideInput'>
                   <div><label>Link</label></div>
-                  <div><input type="text" className='inpBox'  /></div>
+                  <div><input type="text" className='inpBox' id='link' onInput={handleChange}  /></div>
                 </div>
                 <div className='divideInput'>
                   <div><label>Password</label></div>
-                  <div><input type="text" className='inpBox' /></div>
+                  <div><input type="text" className='inpBox' id='password'  onInput={handleChange} /></div>
                 </div>
 
                 <input type="submit" value="Submit" className='submitBtn' />
--- src/components/AdminPages/Lectures.jsx
+++ src/components/AdminPages/Lectures.jsx
@@ -1,39 +1,66 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Lectures = () => {
+
+    const [lecture, setLecture] = useState(
+        {
+            topic:"",
+            creator:"",
+            codingDSA:"",
+            date:"",
+            time:"",
+            link:""
+        }
+    )
+
+    const handleChange = (e) => {
+        const {id, value} = e.target
+       
+        setLecture({...lecture, [id]:value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        // console.log('yes')
+        console.log(lecture)
+    }
+
+
   return (
     <>
         <div className='mainBox'>
             <h1 className='nameHead'>Lectures</h1>
-              <form>
+              <form onSubmit={handleSubmit}>
 
                 <div className='divideInput'>
                   <div><label>Topic Name :</label></div>
-                  <div><input type="text" className='inpBox' /></div>
+                  <div><input type="text" id='topic' className='inpBox' onInput={handleChange}/></div>
                 </div>
 
                 <div className='divideInput'> 
                   <div><label>Creator Name</label></div>
-                  <div><input type="text" className='inpBox'  /></div>
+                  <div><input type="text" id='creator' className='inpBox'  onInput={handleChange}  /></div>
                 </div>
                 <div className='divideInput'>
-                  <div><label>Topic</label></div>
-                  <div><input type="text" className='inpBox' /></div>
+                  <div><label>Topic ( Coding / DSA )</label></div>
+                  <div><input type="text" id='codingDSA' className='inpBox'  onInput={handleChange} /></div>
                 </div>
                 <div className='divideInput'>
                   <div><label>Date</label></div>
-                  <div><input type="Date" className='inpBox'  /></div>
+                  <div><input type="Date" id='date' className='inpBox'  onInput={handleChange}  /></div>
                 </div>
                 <div className='divideInput'>
                   <div><label>Time</label></div>
-                  <div><input type="text" className='inpBox'  /></div>
+                  <div><input type="text" id='time' className='inpBox' onInput={handleChange}  /></div>
                 </div>
                 <div className='divideInput'>
                   <div><label>Link</label></div>
-                  <div><input type="text" className='inpBox'  /></div>
+                  <div><input type="text" id='link' className='inpBox'  onInput={handleChange} /></div>
                 </div>
 
-                <input type="submit" value="Submit" className='submitBtn' />
+                <input type="submit" value="Submit" className='submitBtn'  />
 
   
               </form>
