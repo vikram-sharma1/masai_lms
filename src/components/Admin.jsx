f9d2be51a09fabd95747e73a981d65a7c7b1db43
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-07-23 16:12:55 +0530

    Logout Button in admin page

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -2,11 +2,33 @@ import React from 'react'
 import { useState } from 'react'
 import Assignments from './AdminPages/Assignments'
 import Lectures from './AdminPages/Lectures'
+import { useContext } from "react";
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from './context/AuthContext';
 import './styles/Admin.css'
 
 const Admin = () => {
 
   const [assignment, setAssignment] = useState(true)
+
+  const [showLogout, setShowLogout] = useState(true)
+
+  const navigate = useNavigate()
+
+  const {userName, token, handleLogout} = useContext(AuthContext);
+
+  const handleMouseOver = () => {
+      setShowLogout(false)
+  }
+
+  const mouseLeave = () => {
+      setShowLogout(true)
+  }
+
+  const Logout = () => {
+      handleLogout()
+      navigate('/')
+  }
 
 
   const changeValue = (value) => {
@@ -24,6 +46,11 @@ const Admin = () => {
   return (
     <>
         <h1 className='heading'>Admin Portal</h1>
+        <div className='userBox' onClick={Logout} onMouseOver={handleMouseOver} onMouseLeave={mouseLeave}>
+                    <p className='showName'>Logout</p>
+                    {showLogout ? "" : <p className='adminLogout'>Click to Logout</p> }
+                    
+        </div>
         <div className='AssignmentBox'>
           <div className='addAssig' onClick={()=>{
             changeValue(0)
